0f58037f6e4e98b125437313038be3d8bdd6e994
Donggyu Han <lucky5632@naver.com>
2021-12-29 11:42:10 +0900

    게시판리스트 READ

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,15 +1,36 @@
 package com.study.board.controller;
 
+import com.study.board.Service.BoardService;
+import com.study.board.entity.Board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/") // LocalHost 에서 "/" 경로로 들어왔을경우
-    @ResponseBody // return 을 그대로 띄워주는 것.
-    public String main(){
-        return "Hello World";
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
+    }
+    
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
     }
 }
